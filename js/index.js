ad7cfbd12ec37a0cd0889a21f08dc68c46fabeb7
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-05-14 13:49:27 -0500

    Failed attempt at adding animation to ng-repeat

--- js/index.js
+++ js/index.js
@@ -5,22 +5,23 @@ var App = angular.module('IGSearch', ['ngMessages', 'ngAnimate']);
 
 App.controller('searchCtrl', function($scope, $http, $q) {
 	$scope.resultCount = 0;
-	$scope.query = "";
-	$scope.results = [];
-	$scope.searchDone = DEBUG? true : false;
-	$scope.searching = DEBUG? true : false;
-	$scope.searchFail = DEBUG? true : false;
-	$scope.lastSearch = "";
+	$scope.query       = "";
+	$scope.results     = [];
+	$scope.searchDone  = DEBUG? true : false;
+	$scope.searching   = DEBUG? true : false;
+	$scope.searchFail  = DEBUG? true : false;
+	$scope.lastSearch  = "";
 	//
 	$scope.searchPics = function() {
 		if(DEBUG) console.log('making request to instagram');
 		if(DEBUG) console.log($scope.igSearchForm);
 		//
-		$scope.searching = true;
 		$scope.searchDone = false;
 		$scope.searchFail = false;
 		//
 		if ($scope.igSearchForm.$valid) {
+			$scope.searching = true;
+			//
 			$http({
 				method: 'JSONP',
 				url: "https://api.instagram.com/v1/tags/" + $scope.query + "/media/recent",
@@ -33,25 +34,26 @@ App.controller('searchCtrl', function($scope, $http, $q) {
 					if(DEBUG) console.log('success');
 					if(DEBUG) console.log(data);
 					//
-					$scope.results = data.data;
-					$scope.searchDone = true;
+					$scope.results    = data.data;
 					$scope.lastSearch = $scope.query;
-					$scope.query      = "";
 					//
 					$scope.igSearchForm.$setPristine();
 					$scope.igSearchForm.$setUntouched();
+					$scope.searching  = false;
+					$scope.searchDone = true;
+					$scope.query      = "";
 					queryFocus();
 				})
 				.error(function(data, status){
 					if(DEBUG) console.log(status);
 					//
 					$scope.searchFail = true;
+					$scope.searching  = false;
 				});
 		}
 		else {
 			queryFocus();
 		}
-		$scope.searching = false;
 	};
 });
 
